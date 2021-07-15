d62a69185efbc11be281d02656e8e6356fb31321
Angadsingh07 <86396719+Angadsingh07@users.noreply.github.com>
2021-07-15 23:38:16 +0530

    new update

--- My Cloud EMS/mainApp.c
+++ My Cloud EMS/mainApp.c
@@ -1,4 +1,14 @@
 #include<conio.h>
+
+FILE *fPtr
+
+struct studentInfo{
+    char dgreName[15], brnchName[40], spclName[50], subName[50], subType[10];
+    int subCode;
+}
+
 void main{
-    
+
+printf("HELLO");
+
 }
