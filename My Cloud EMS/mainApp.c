e4993a43d91f673fc7aa5fd61ab0720436d4b7cc
Omkar Bhattarai <83543183+omkarbhattarai71@users.noreply.github.com>
2021-07-16 00:01:21 +0545

    see_you_again
    new_test

--- My Cloud EMS/mainApp.c
+++ My Cloud EMS/mainApp.c
@@ -1,4 +1,18 @@
 #include<conio.h>
 void main{
-    
+
+} 
+
+FILE *fPtr
+
+struct studentInfo
+{
+    char dgreName[15], brnchName[40], spclName[50], subName[50], subType[10];
+    int subCode;
+}
+
+void main
+{
+    printf("Hello Buddy, We need to look more, upon github functioning and coding stuffs");
+
 }
